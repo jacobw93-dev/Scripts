afb55ddb43d18aa88097ae2f87fc36a3677acb24
jacobw93-dev <jacobw93-dev@users.noreply.github.com>
2020-09-29 09:59:10 +0200

    Update Bulk_rename_files_v1.ps1

--- Bulk_rename_files_v1.ps1
+++ Bulk_rename_files_v1.ps1
@@ -1,20 +1,23 @@
 cd D:\Downloads\ ;
-$target_path = (Get-Location).path + '\' + 'Folder001';
-$ext = '.mp3';
-$ext_mask = '*' + $ext;
-$filter_mask = '*xyz*';
-$array_ext = @('*.txt','*.nfo','*.exe');
+$input_folder = Read-Host -Prompt "Podaj nazwê katalogu, do którego przenieœæ przetworzone pliki `n";
+$input_folder = (Get-Location).path + '\' + $input_folder;
+$input_ext = Read-Host -Prompt "Podaj nazwê rozszerzenia dla plików do przetworzenia `n";
+$input_ext = '.' + $input_ext;
+$file_filter = '*' + $ext;
+$dir_filter = Read-Host -Prompt "Podaj maskê dla katalogów do przetworzenia `n";
+$dir_filter = '*' + $dir_filter + '*';
+$array_ext = @('*.txt','*.nfo','*.exe','*.diz');
 $regex_str1 = '[^0-9A-Za-z\.\[\]]';
 $regex_str2 = '\.+';
 
-if(!(Test-Path $target_path)) {
+if(!(Test-Path $input_folder)) {
     
-    mkdir $target_path
+    mkdir $input_folder
 
 }
 
-dir -Directory -filter $filter_mask | move-item -Destination $target_path;
-cd $target_path    
+dir -Directory -filter $dir_filter | move-item -Destination $input_folder;
+cd $input_folder    
 
 $filesandfolders = Get-ChildItem -recurse | Where-Object {$_.name -match $regex_str1} 
 $filesandfolders | Where-Object {!$_.PsIscontainer}  |  foreach {
@@ -26,17 +29,18 @@ $filesandfolders | Where-Object {$_.PsIscontainer}  |  foreach {
     Rename-Item -Literalpath $_.Fullname -newname $New -passthru
 }
 
-$Folder = dir -Recurse -Directory -filter $filter_mask ;
+$Folder = dir -Recurse -Directory -filter $dir_filter ;
           
 Foreach ($dir In $Folder) 
     {
-    $Count = Get-ChildItem -File -LiteralPath $dir -Filter $ext_mask | Measure-Object | %{$_.Count}
+	$current_dir = (Get-Location).path + '\' + $dir;
+    $Count = Get-ChildItem -File -LiteralPath $current_dir -Filter $file_filter | Measure-Object | %{$_.Count}
    
     # Set default value for addition to file name 
     $i = 1 
-    $newdir = $dir.name + "_" 
+    $newdir = $dir.name + "." 
     # Search for the files set in the filter
-    $files = Get-ChildItem -LiteralPath $dir.fullname -Filter $ext_mask -Recurse
+    $files = Get-ChildItem -LiteralPath $dir.fullname -Filter $file_filter -Recurse
     Foreach ($file In $files) 
         { 
         # Check if a file exists 
@@ -44,24 +48,23 @@ Foreach ($dir In $Folder)
             { 
             # Split the name and rename it to the parent folder 
             $split    = $file.name.split($ext)
-                if ($Count > 1 )
+                if ( $Count -gt 1 )
                     { $replace  = $split[0] -Replace $split[0],($newdir + $i + $ext) }
                 else
-                    { $replace  = $split[0] -Replace $split[0],($dir.name + $ext) }
- 
-            # Trim spaces and rename the file 
+					{ $replace  = $split[0] -Replace $split[0],($dir.name + $ext) }
+			# Trim spaces and rename the file 
             $image_string = $file.fullname.ToString().Trim() 
             #"$split[0] renamed to $replace" 
-            Rename-Item -LiteralPath "$image_string" "$replace" ;
+            Rename-Item -LiteralPath "$image_string" "$replace";
             $i++ 
             } 
         } 
     }
 
-dir -Recurse -Directory -filter $filter_mask | dir -Recurse -File -filter $ext_mask | Move-Item -Destination .\ ;
-dir -Recurse -file -filter $filter_mask | Rename-item -NewName {$_.Name -replace "$regex_str2","."} ;
-for($i=0;$i -le 2;$i++){dir -Recurse -Directory -filter $filter_mask | dir -Recurse -File -Filter $($array_ext[$i]) | Remove-Item};
-ls -Directory -filter $filter_mask| where { -NOT $_.GetFiles()} | Remove-Item;
+dir -Recurse -Directory -filter $dir_filter | dir -Recurse -File -filter $file_filter | Move-Item -Destination .\ ;
+dir -Recurse -file -filter $dir_filter | Rename-item -NewName {$_.Name -replace "$regex_str2","."} ;
+for($i=0;$i -le 2;$i++){dir -Recurse -Directory -filter $dir_filter | dir -Recurse -File -Filter $($array_ext[$i]) | Remove-Item};
+ls -Directory -filter $dir_filter| where { -NOT $_.GetFiles()} | Remove-Item;
 
 start . ;
 cd \ ;

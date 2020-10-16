ce8d175dd4e5b98d8ba9da5fd516b7abd4c276e8
jacobw93-dev <62847554+jacobw93-dev@users.noreply.github.com>
2020-10-16 14:29:55 +0200

    Add files via upload

--- SAP_LOGON_config.bat
+++ SAP_LOGON_config.bat
@@ -2,6 +2,7 @@
 cmd /c
 popd
 cls
+NET USE * /delete /y > nul
 pushd %~dp0
 SETLOCAL ENABLEEXTENSIONS EnableDelayedExpansion
 mode 100,20 > nul
@@ -16,28 +17,33 @@ IF ERRORLEVEL ==1 GOTO :local
 IF ERRORLEVEL ==2 GOTO :network
 
 :local
-powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/s /c "echo:Kopiowanie pliku %CD%\SAPLOGON.ini do %WINDIR%\*.*" & "copy /Y " "%CD%\SAPLOGON.ini" " " "%WINDIR%\*.*" & pause & exit'"
+powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/s /c "echo:Kopiowanie pliku %CD%\SAPLOGON.ini do %WINDIR%\*.*" & "copy /Y " "%CD%\SAPLOGON.ini" " " "%WINDIR%\*.*" & "echo:Naciśnij [Spacja] lub [Enter] aby kontynuować..." & pause > nul & exit'"
 goto :CONTINUE
 
 :network
 pushd %NETPATH%
 set "currentpath=%CD%"
-powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/s /c "pushd %~dp0" & "pushd %NETPATH%" & "echo:Kopiowanie pliku %CD%\SAPLOGON.ini do %WINDIR%\*.*" & "copy /Y " "%CD%\SAPLOGON.ini" " " "%WINDIR%\*.*" & pause & popd & exit'"
+powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/s /c "pushd %~dp0" & "pushd %NETPATH%" & "echo:Kopiowanie pliku %CD%\SAPLOGON.ini do %WINDIR%\*.*" & "copy /Y " "%CD%\SAPLOGON.ini" " " "%WINDIR%\*.*" & "echo:Naciśnij [Spacja] lub [Enter] aby kontynuować..." & pause > nul & popd & exit'"
 goto :CONTINUE
 
 :CONTINUE
-pause
+echo.
+echo:"Naciśnij [Spacja] lub [Enter] aby kontynuować..."
+pause > nul
 echo.
 echo:Utworzenie wpisu w rejestrze na podstawie pliku %currentpath%\SAPSecureLogin.reg
 %windir%\System32\reg.exe import %currentpath%\SAPSecureLogin.reg
-pause
+echo:"Naciśnij [Spacja] lub [Enter] aby kontynuować..."
+pause > nul
 echo.
 echo:Kopiowanie pliku "%currentpath%\SAPLOGON.ini" do "%programfiles(x86)%\sap\SAPLOGON_INI\*.*"
 copy /Y "%currentpath%\SAPLOGON.ini" "%programfiles(x86)%\sap\SAPLOGON_INI\*.*"
-pause
+echo:"Naciśnij [Spacja] lub [Enter] aby kontynuować..."
+pause > nul
 echo.
 echo:Kopiowanie pliku "%currentpath%\Ao_user_roaming.config" do "%appdata%\SAP\Cof\*.*"
 copy /Y "%currentpath%\Ao_user_roaming.config" "%appdata%\SAP\Cof\*.*"
-pause
+echo:"Naciśnij [Spacja] lub [Enter] aby kontynuować..."
+pause > nul
 popd
 

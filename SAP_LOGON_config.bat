d7c3474fb993aea8ad8bae1835fa435c03724a0b
jacobw93-dev <jacobw93-dev@users.noreply.github.com>
2020-11-20 15:50:46 +0100

    Update SAP_LOGON_config.bat

--- SAP_LOGON_config.bat
+++ SAP_LOGON_config.bat
@@ -1,36 +1,69 @@
 @echo off
+
 cmd /c
-NET USE * /delete /y > nul
-popd
-cls
-pushd %~dp0
 
 SETLOCAL ENABLEEXTENSIONS EnableDelayedExpansion
-mode 100,20 > nul
+
+NET USE * /delete /y > nul
+
+popd
+
+cls
+
+pushd %~dp0
+
+mode 120,30 > nul
+
 chcp 65001 > nul
+
+ 
 
 set "cust_pause=Naciśnij [Spacja] lub [Enter] aby kontynuować..."
 
+FOR /F "skip=2 tokens=3*" %%A IN ('%windir%\System32\reg.exe query HKEY_CURRENT_USER\Software\SAP\SAPLogon\ConfigFilesLastUsed /v ConnectionConfigFile') do (set "V_SAPLOGON=%%A %%B")
+
+ 
+
 echo:Utworzenie wpisu w rejestrze na podstawie pliku %CD%\SAPSecureLogin.reg
+
 %windir%\System32\reg.exe import %CD%\SAPSecureLogin.reg
+
 echo:"%cust_pause%"
+
 pause > nul
 
-echo.
-echo:Kopiowanie pliku "%CD%\SAPLOGON.ini" do "%programfiles(x86)%\sap\SAPLOGON_INI\*.*"
-copy /Y "%CD%\SAPLOGON.ini" "%programfiles(x86)%\sap\SAPLOGON_INI\*.*"
-echo:"%cust_pause%"
-pause > nul
+ 
 
 echo.
+
+echo:Kopiowanie pliku "%CD%\SAPLOGON.ini" do "%V_SAPLOGON%"
+
+copy /Y "%CD%\SAPLOGON.ini" "%V_SAPLOGON%"
+
+echo:"%cust_pause%"
+
+pause > nul
+
+ 
+
+echo.
+
 echo:Kopiowanie pliku "%CD%\Ao_user_roaming.config" do "%appdata%\SAP\Cof\*.*"
+
 copy /Y "%CD%\Ao_user_roaming.config" "%appdata%\SAP\Cof\*.*"
+
 echo:"%cust_pause%"
+
 pause > nul
+
+ 
 
 echo.
-powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/s /k "echo:Kopiowanie pliku %CD%\SAPLOGON.ini do %WINDIR%\*.*" & "copy /Y " "%CD%\SAPLOGON.ini" " " "%WINDIR%\*.*" & "echo:%cust_pause%" & pause > nul & popd & exit'"
-echo:"Naciśnij [Spacja] lub [Enter] aby zakończyć"
-pause > nul
-popd
 
+powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/s /k "echo:Kopiowanie pliku %CD%\SAPLOGON.ini do %WINDIR%\*.*" & "copy /Y " "%CD%\SAPLOGON.ini" " " "%WINDIR%\*.*" & "echo:%cust_pause%" & pause > nul & popd & exit'"
+
+echo:"%cust_pause%"
+
+pause > nul
+
+popd

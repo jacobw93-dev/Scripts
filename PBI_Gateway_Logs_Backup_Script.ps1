8cf3cabae7207c7ed53ee1d17f1bcb8c3e3991fe
jacobw93-dev <62847554+jacobw93-dev@users.noreply.github.com>
2024-05-23 10:45:47 +0200

    Update PBI_Gateway_Logs_Backup_Script.ps1

--- PBI_Gateway_Logs_Backup_Script.ps1
+++ PBI_Gateway_Logs_Backup_Script.ps1
@@ -3,15 +3,32 @@ hostname
 
 $Days = 7
 
+$sourcePath = 'D:\XJAKA1108\'
+
 # Define arrays for source and target folders
 $Sourcefolders = @("\\VSSAPMSG01S\Users\svc_MSDataGatewaySbx\AppData\Local\Microsoft\On-premises data gateway\",
                   "\\VSSAPMSG01P\Users\svc_MSDataGateway\AppData\Local\Microsoft\On-premises data gateway\")
-$Targetfolders = @("D:\XJAKA1108\PBI_Gateway_VSSAPMSG01S\",
-                  "D:\XJAKA1108\PBI_Gateway_VSSAPMSG01P\")
+$Targetfolders = @(($sourcePath+"PBI_Gateway_VSSAPMSG01S\"),
+                  ($sourcePath+"PBI_Gateway_VSSAPMSG01P\"))
+
+# Get the current date in YYYY-MM-DD format
+$CurrentDate = Get-Date -Format "yyyy-MM-dd"
+
+# Define the output ZIP file path with the current date
+$OutputZipPath = ($sourcePath + "PBI_Gateways_logs_Archive_$CurrentDate.zip")
+
+# Remove the existing ZIP file if it exists
+if (Test-Path $OutputZipPath) {
+    Remove-Item $OutputZipPath
+}
+			  
+if (Test-Path -Path $sourcePath -PathType Container) {
+    gci $sourcePath -Recurse -File | Remove-Item -Verbose
+}
 
 # Function to perform the file copy operations
 function CopyFiles($Sourcefolder, $Targetfolder) {
-    $LogFilesSize = "{0:N2} GB" -f ((Get-ChildItem -Path $Sourcefolder -Recurse | Measure-Object -Property Length -Sum).Sum / 1GB)
+    $LogFilesSize = "{0:N2} GB" -f ((Get-ChildItem -Path $Sourcefolder -Recurse | Where-Object {($_.LastWriteTime -gt [datetime]::Now.AddDays(-$Days)) -and ($_.FullName -notlike "*\Spooler\*")} | Measure-Object -Property Length -Sum).Sum / 1GB)
     Write-Host "`n$LogFilesSize`n" -ForegroundColor Yellow
     
     $query = Get-ChildItem $Sourcefolder -Recurse -Exclude "Spooler" | Where-Object {($_.LastWriteTime -gt [datetime]::Now.AddDays(-$Days)) -and ($_.FullName -notlike "*\Spooler\*")}
@@ -33,3 +50,5 @@ function CopyFiles($Sourcefolder, $Targetfolder) {
 for ($i = 0; $i -lt $Sourcefolders.Count; $i++) {
     CopyFiles $Sourcefolders[$i] $Targetfolders[$i]
 }
+
+Compress-Archive -Path ($sourcePath + "*") -DestinationPath $OutputZipPath
